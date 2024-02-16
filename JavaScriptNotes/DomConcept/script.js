729db6f6a5f71b70930a8804d79197a993516617
Shubham Dhatwalia <shubhamdhatwalia3@gmail.com>
2024-02-16 17:02:42 +0530

    practice

---
+++ JavaScriptNotes/DomConcept/script.js
@@ -0,0 +1,10 @@
+console.dir(document.body.childNodes[1]);
+console.log(document.body);
+document.body.style.background ="skyblue";
+document.body.childNodes[3].innerText = "abcd";
+
+
+// DOM Manipulation -----------------------------------------------
+
+// Selecting wiht id --------------------
+
