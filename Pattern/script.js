cd987f55dabc70fcbfe65aec7a80dbbc2a5c4b95
Shubham Dhatwalia <shubhamdhatwalia3@gmail.com>
2024-04-16 23:33:06 +0530

    practice

--- Pattern/script.js
+++ Pattern/script.js
@@ -24,24 +24,60 @@
 
 
 
-let n =8;
+// let n =8;
 
-for(let i=0; i<n; i++){
-    let pattern ='';
-    for(let j=0; j<n; j++){
+// for(let i=0; i<n; i++){
+//     let pattern ='';
+//     for(let j=0; j<n; j++){
 
-        if(i==0 || i == Math.floor(n/2) || i==n-1){
-            pattern += 'S';
-        }
-        else if(j==0 && i<Math.floor(n/2)){
-            pattern += 'S';
-        }
-        else if(j==n-1 && i>Math.floor(n/2)){
-            pattern += 'S';
-        }
-        else{
-        pattern += ' ';
-        }
-    }
-    console.log(pattern);
+//         if(i==0 || i == Math.floor(n/2) || i==n-1){
+//             pattern += 'S';
+//         }
+//         else if(j==0 && i<Math.floor(n/2)){
+//             pattern += 'S';
+//         }
+//         else if(j==n-1 && i>Math.floor(n/2)){
+//             pattern += 'S';
+//         }
+//         else{
+//         pattern += ' ';
+//         }
+//     }
+//     console.log(pattern);
+// }
+
+
+
+let name = prompt("Enter Name:");
+let department = prompt("Enter department :");
+
+let dept = department.toLowerCase();
+
+switch(dept){
+    case 'hr' :
+        console.log(name+" your seat is on Ground floor");
+        break;
+
+     case 'mobile' :
+        console.log(name+" your seat is on 4th floor");
+        break;
+        
+     case 'digit marketing' :
+        console.log(name + " your seat is on 1st floor");
+        break;
+
+     case 'seo' :
+        console.log(name + " your seat is on 2nd floor");
+        break;
+
+     case 'web development' :
+        console.log(name +" your  seat is right side on 3rd floor");
+        break;
+
+     case 'web intern' :
+            console.log(name+" your seat is left side on 3rd floor");
+            break;
+
+     default :
+            console.log("Invalid input");
 }
